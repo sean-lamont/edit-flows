e0fafc4eb41b39a85f9d8f8b28a6b8fefdc73b67
sean-lamont <sean.a.lamont@outlook.com>
2025-10-16 02:50:43 +0000

    Update data prep to better format.

--- adapted_model.py
+++ adapted_model.py
@@ -4,23 +4,43 @@ import torch.nn.functional as F
 from transformers import AutoModelForCausalLM
 import transformers
 from utils import SinusoidalTimeEmbedding
-from adapt_qwen import qwen_new_forward
+# from adapt_qwen import qwen_new_forward
+from torch.nn.attention.flex_attention import create_block_mask, and_masks, or_masks, create_mask
 
 
+def causal_mask(b,h,q_idx,kv_idx):
+    return q_idx >= kv_idx
 
-def get_anneal_attn_mask(seq_len, bsz, dtype, device, attn_mask_ratio):
-    mask = torch.full((seq_len, seq_len), 0, device=device)
-    mask_cond = torch.arange(mask.size(-1), device=device)
-    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 1)
-    causal_mask = mask.to(dtype)
+def create_padding_mask(pads):
+    def padding(b, h, q_idx, kv_idx):
+        return ~pads[b, q_idx] & ~pads[b, kv_idx]
+    return padding
 
-    random_mask = torch.bernoulli(torch.full((seq_len, seq_len), 0.0, device=device) + attn_mask_ratio)
+def create_random_mask(attn_ratio, seq_len):
+    random_mask = (torch.rand(seq_len, seq_len) < attn_ratio).to('cuda')
 
-    anneal_mask = torch.logical_or(causal_mask, random_mask)
-    expanded_mask = anneal_mask[None, None, :, :].expand(bsz, 1, seq_len, seq_len)
-    inverted_mask = 1.0 - expanded_mask.to(dtype)
+    def random_mask_func(b, h, q_idx, kv_idx):
+        return random_mask[q_idx][kv_idx]
+    return random_mask_func
 
-    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+
+#
+# def get_anneal_attn_mask(seq_len, bsz, dtype, device, attn_mask_ratio):
+#     def create_random_mask(b, h, q_idx, kv_idx):
+#         return random_mask[q_idx][kv_idx]
+#
+#     mask = torch.full((seq_len, seq_len), 0, device=device)
+#     mask_cond = torch.arange(mask.size(-1), device=device)
+#     mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 1)
+#     causal_mask = mask.to(dtype)
+#
+#     random_mask = torch.bernoulli(torch.full((seq_len, seq_len), 0.0, device=device) + attn_mask_ratio)
+#
+#     anneal_mask = torch.logical_or(causal_mask, random_mask)
+#     expanded_mask = anneal_mask[None, None, :, :].expand(bsz, 1, seq_len, seq_len)
+#     inverted_mask = 1.0 - expanded_mask.to(dtype)
+#
+#     return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
 
 
 
@@ -29,10 +49,11 @@ class AdaptedEditFlowsTransformer(nn.Module):
     def __init__(self, pretrained_model_name: str, hidden_dim=512):
         super().__init__()
 
-        transformers.models.qwen2.modeling_qwen3.Qwen3Model.forward = qwen_new_forward
         # godel_id = "Goedel-LM/Goedel-Prover-V2-8B"
-        self.model = AutoModelForCausalLM.from_pretrained(pretrained_model_name, torch_dtype=torch.bfloat16, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(pretrained_model_name, dtype=torch.bfloat16, trust_remote_code=True,
+                                                          _attn_implementation='flex_attention')
 
+        self.model.compile()
 
         self.vocab_size = self.model.config.vocab_size
         self.time_emb = SinusoidalTimeEmbedding(hidden_dim)
@@ -50,15 +71,35 @@ class AdaptedEditFlowsTransformer(nn.Module):
     def forward(self, tokens: torch.Tensor, t: torch.Tensor, pad_mask: torch.Tensor, attn_mask_ratio: float = 0.0):
         B, L = tokens.shape
 
-        anneal_mask = get_anneal_attn_mask(L, B, dtype=self.model.dtype, device=tokens.device, attn_mask_ratio=attn_mask_ratio)
+        # anneal_mask = get_anneal_attn_mask(L, B, dtype=self.model.dtype, device=tokens.device, attn_mask_ratio=attn_mask_ratio)
 
 
         # update the 4D anneal mask with pad_mask to account for padding tokens, so nothing attends to pads. Note that pad_mask is True for pad tokens
-        anneal_mask = anneal_mask.masked_fill(pad_mask[:, None, None, :], torch.finfo(self.model.dtype).min)
+        # anneal_mask = anneal_mask.masked_fill(pad_mask[:, None, None, :], torch.finfo(self.model.dtype).min)
+
+        padding_mask = create_padding_mask(pad_mask)
+        if attn_mask_ratio < 1.0:
+            #compute block for annealed attention
+            random_func = create_random_mask(attn_mask_ratio, L)
+            or_mask = or_masks(*[causal_mask, random_func])
+            final_mask = and_masks(*[or_mask, padding_mask])
+        else:
+            # only compute padded attention block
+            final_mask = padding_mask
+        block_mask = create_block_mask(final_mask, B, None,L,L, device='cuda')#, _compile=True)
 
 
-        outputs = self.model(input_ids=tokens, attention_mask=anneal_mask, output_hidden_states=True)
-        
+        outputs = self.model(input_ids=tokens, attention_mask=block_mask, output_hidden_states=True,
+                             kernel_options = {
+            "BLOCK_M": 32,
+            "BLOCK_N": 32,
+            "BLOCK_M1": 32,
+            "BLOCK_N1": 32,
+            "BLOCK_M2":
+            "BLOCK_N2": 32,
+        })
+
+
         hidden_states = outputs.hidden_states[-1]
         
         time = self.time_emb(t).unsqueeze(1).expand(-1, L, -1)
--- train.py
+++ train.py
@@ -1,14 +1,21 @@
 import lightning.pytorch as pl
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from adapted_model import AdaptedEditFlowsTransformer
 from datamodule import AdaptedDataModule
 from adapt_qwen import create_adapted_qwen_model
 from adapted_lightning import  AdaptedLitModule
 
 def main():
     model_id = "Goedel-LM/Goedel-Prover-V2-8B"
-    model, tokenizer = create_adapted_qwen_model(model_id)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
 
     dm = AdaptedDataModule(tokenizer=tokenizer, batch_size=2)
 
+    model = AdaptedEditFlowsTransformer(model_id)
     lit_module = AdaptedLitModule(model, len(tokenizer), tokenizer.pad_token_id, 151651) #using <|quad_end|>
 
     trainer = pl.Trainer(max_epochs=1, log_every_n_steps=1)
